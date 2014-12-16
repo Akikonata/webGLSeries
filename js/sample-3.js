705bde41658ce3a2abb7d971bd99a8e50f2d29c9
panzheng <panzheng@baidu.com>
2014-12-16 20:10:33 +0800

    helper

--- js/sample-3.js
+++ js/sample-3.js
@@ -8,28 +8,30 @@
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(width, height);
 	document.body.appendChild(renderer.domElement);
+	//创建指针
+	var dir = new THREE.Vector3(1, 0, 0);
+	var origin = new THREE.Vector3(0, 0, 0);
+	var length = 10;
+	var hex = 0xffffff;
 
-	var material = new THREE.MeshBasicMaterial({
-		color: 0x0000ff
-	});
+	var arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
+	arrowHelper.rotation.x = 1;
+	scene.add(arrowHelper);
+	//创建表盘
 
-	var radius = 5;
-	var segments = 32;
-	var thetaStart = 0;
-	var thetaLength = Math.PI / 2;
-
-	var circleGeometry = new THREE.CircleGeometry(radius, segments, thetaStart, thetaLength);
-	var circle = new THREE.Mesh(circleGeometry, material);
-	scene.add(circle);
+	//设置相机的位置
+	camera.position.y = 0;
 	camera.position.z = 10;
-	renderer.render(scene, camera);
 
-	// function animate() {
-	// 	circle.rotation.y += 0.01;
-	// 	// camera.position.x = 50 * Math.cos(Radius += Math.PI / 180);
-	// 	// camera.position.z = 506 * Math.sin(Radius);
-	// 	renderer.render(scene, camera);
-	// 	requestAnimationFrame(animate);
-	// }
-	// animate();
+	function animate() {
+		arrowHelper.rotation.y += Math.PI / 30;
+		renderer.render(scene, camera);
+		setTimeout(animate, 1000);
+	}
+	animate();
+	window.onresize = function() {
+		width = document.body.clientWidth;
+		height = document.body.clientHeight;
+		renderer.setSize(width, height);
+	};
 })();
