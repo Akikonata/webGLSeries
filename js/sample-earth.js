07a48049eed0199345beb48383df64c7045b90f3
panzheng <panzheng@baidu.com>
2014-12-16 13:07:34 +0800

    earth

--- js/sample-earth.js
+++ js/sample-earth.js
@@ -1,29 +1,44 @@
 (function() {
+	//初始化场景
 	var scene = new THREE.Scene();
-	var width = document.body.clientHeight;
-	var height = document.body.clientWidth;
+	var width = document.body.clientWidth;
+	var height = document.body.clientHeight;
+	//视锥角度、视野纵横比、近平面，远平面
 	var camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000);
 	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize(height, width);
+	renderer.setSize(width, height);
 	document.body.appendChild(renderer.domElement);
-	//初始化球体和材质
+	//往场景里添加物体
 	var geometry = new THREE.SphereGeometry(10, 32, 32);
 	var texture = THREE.ImageUtils.loadTexture("texture.png");
 	var material = new THREE.MeshPhongMaterial({
 		color: 0xFFFFFF,
 		map: texture
 	});
-	var earth = new THREE.Mesh(geometry, material);
-	scene.add(earth);
 	var pointLight = new THREE.PointLight(0xFFFFFF);
 	pointLight.position.set(0, 0, 0);
 	scene.add(pointLight);
+	//漫反射
 	var light = new THREE.AmbientLight(0xFFFFFF);
 	scene.add(light);
+
+	var earth = new THREE.Mesh(geometry, material);
+	earth.position.z = -20;
+	scene.add(earth);
+
+	camera.position.y = 0;
+	camera.position.z = 0;
+	var Radius = 0;
+
+	function animate() {
+		earth.rotation.y += 0.01;
+		renderer.render(scene, camera);
+		requestAnimationFrame(animate);
+	}
+	animate();
 	window.onresize = function() {
-		width = document.body.clientHeight;
-		height = document.body.clientWidth;
-		renderer.setSize(height, width);
+		width = document.body.clientWidth;
+		height = document.body.clientHeight;
+		renderer.setSize(width, height);
 	};
-	renderer.render(scene, camera);
 })();
