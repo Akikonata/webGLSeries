95305839f8d5c969ab172206729680b086103caa
panzheng <panzheng@baidu.com>
2014-12-18 19:51:06 +0800

    clock

--- js/sample-3.js
+++ js/sample-3.js
@@ -24,17 +24,33 @@
 	ring.rotation.x = -Math.PI / 2;
 	scene.add(ring);
 	//创建秒针
-	var secondHandGeometry = new THREE.BoxGeometry(9, 1.5, 1);
+	var secondHandGeometry = new THREE.BoxGeometry(9.5, 1.5, 0.5);
 	var secondHandMaterial = new THREE.MeshBasicMaterial({
 		color: 0x000000
 	});
 	var secondHand = new THREE.Mesh(secondHandGeometry, secondHandMaterial);
 	scene.add(secondHand);
+	//创建刻度
+	var carveGeometry = new THREE.BoxGeometry(1, 1, 1);
+	var carveMaterial = new THREE.MeshBasicMaterial({
+		color: 0x000000
+	});
+	for (var i = 0; i < 12; i++) {
+		var carve = new THREE.Mesh(carveGeometry, carveMaterial);
+		carve.rotation.y = Math.PI * i / 6;
+		carve.position.x = 9 * Math.cos(carve.rotation.y);
+		carve.position.z = -9 * Math.sin(carve.rotation.y);
+		scene.add(carve);
+	}
+	//创建中心的圆柱
 	//设置相机的位置
-	camera.position.y = 5;
-	camera.position.z = 20;
+	camera.position.y = 12;
+	camera.position.z = 25;
 
 	function animate() {
+		secondHand.rotation.y -= Math.PI / 30;
+		secondHand.position.x = 4.5 * Math.cos(secondHand.rotation.y);
+		secondHand.position.z = -4.5 * Math.sin(secondHand.rotation.y);
 		renderer.render(scene, camera);
 		setTimeout(animate, 1000);
 	}
