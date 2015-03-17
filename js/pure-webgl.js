cf0140e2149c1c279733a9cb295a13b65442bfbd
panzheng <panzheng@baidu.com>
2015-03-18 02:51:52 +0800

    pure

--- js/pure-webgl.js
+++ js/pure-webgl.js
@@ -1,1 +1,8 @@
-(function() {})();
+(function() {
+	var initWebGL = function() {
+		var gl = canvas.getContext("experimental-webgl");
+		return gl;
+	};
+	var gl = initWebGL();
+	console.log(gl);
+})();
