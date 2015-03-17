1e1dfde0acf868fca70ece1b7e7a5d84e494f007
panzheng <panzheng@baidu.com>
2015-03-18 02:55:20 +0800

    viewport

--- js/pure-webgl.js
+++ js/pure-webgl.js
@@ -3,6 +3,9 @@
 		var gl = canvas.getContext("experimental-webgl");
 		return gl;
 	};
+	var initViewport = function(gl, canvas) {
+		gl.viewport(0, 0, canvas.width, canvas.height);
+	};
 	var gl = initWebGL();
-	console.log(gl);
+	initViewport(gl, canvas);
 })();
